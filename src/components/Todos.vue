ceee6810290b41cc4f84ac2114d6affc9806e6a1
Mitch Gavan <mitchgavan@gmail.com>
2017-11-01 07:37:26 +1100

    add: todo app markup/style

---
+++ src/components/Todos.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="todoapp">
+
+    <header class="header">
+			<h1>todos</h1>
+			<input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?">
+		</header>
+
+    <section class="main">
+      <ul class="todo-list">
+    		<li class="todo">
+    			<div class="view">
+    				<input class="toggle" type="checkbox">
+    				<label></label>
+    				<button class="destroy"></button>
+    			</div>
+    			<input class="edit" type="text">
+    		</li>
+    	</ul>
+    </section>
+
+    <footer class="footer">
+			<span class="todo-count">
+				<strong></strong>1 item left
+			</span>
+			<ul class="filters">
+				<li><a href="#/all">All</a></li>
+				<li><a href="#/active">Active</a></li>
+				<li><a href="#/completed">Completed</a></li>
+			</ul>
+			<button class="clear-completed">
+				Clear completed
+			</button>
+		</footer>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
